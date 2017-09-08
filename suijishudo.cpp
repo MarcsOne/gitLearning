3e230e65ec50ef4e6a90f5ca3d23948d31c476ed
Asshead <921737326@qq.com>
2017-09-08 12:43:14 +0800

    the second think

--- suijishudo.cpp
+++ suijishudo.cpp
@@ -4,61 +4,18 @@
 #include<ctime>
 #include<vector>
 #include<fstream>
-/*
-9x9数独按照3x3矩阵可以从上到下从左到右编号为1,2,3,4,5,6,7,8,9号矩阵
-这个随机数独生成方法是先随机生成1,5,9号矩阵，然后通过回溯法分别去生成3,7,2,4,6,8号矩阵
-然而这个方法实践了很久，尚未写出合适的代码，以下代码只是实现的一部分，仍有错误需要修改
-*/
-
 using namespace std;
-
+/*
+由于上一代码实践了将近1个下午，都没有成功，因此以下代码仅使用回溯法来生成随机数独矩阵
+*/
 const int SIZE = 9;
 vector<vector <int> > num(SIZE, vector<int>(SIZE));
 int temp[SIZE];
 
+ofstream ocout;//定义输出流的一个对象
 
-void generate1to9() {
 
-	for (int k = 0; k < 9; k++) {
-		temp[k] = 0;
-	}//初始化数组 
-	for (int i = 0; i<9; i++) {
-		temp[i] = 1 + rand() % 9;                //得到随机数(范围在1-9之间)
-		for (int j = 0; j<i; j++)                 //判断和前面的数是否重复
-			if (temp[i] == temp[j]) { i--; break; }  //如果重复,重新产生随机数
-	}//产生9个随机数
 
-}
-// 随机产生1-9不重复的数，结果填到temp数组中 
-
-void init() {
-	generate1to9();
-	for (int i = 0, k = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++, k++) {
-			if (i == 0 && j == 0) num[i][j] = (3 + 2) % 9 + 1;
-			else {
-
-				if (temp[k] == 6) k++;
-				num[i][j] = temp[k];
-			}
-		}
-	}
-
-	generate1to9();
-	for (int i = 3, k = 0; i < 6; i++) {
-		for (int j = 3; j < 6; j++, k++) {
-			num[i][j] = temp[k];
-		}
-	}
-
-	generate1to9();
-	for (int i = 6, k = 0; i < 9; i++) {
-		for (int j = 6; j < 9; j++, k++) {
-			num[i][j] = temp[k];
-		}
-	}
-}
-// 随机初始化编号为1,5,9的中间三个3*3矩阵
 bool IsSuitable(int row, int col) {
 	int n = num[row][col];
 	for (int i = 0; i < row; i++) {
@@ -88,63 +45,87 @@ bool IsSuitable(int row, int col) {
 			j++;
 		}
 	}  return true;
-}
+}  																	//判别该数填在该位置是否合适 
 
-bool generate(int beginrow, int begincol, int endrow, int endcol) {
-	int row = beginrow;
-	int col = begincol;
+bool generate(int row, int col) {
+	int nextrow, nextcol;
 	vector<int> number;
-	for (int i = 1; i <= 9; i++)
-		number.push_back(i);
+	if (row == 0 && col == 0) number.push_back((3 + 2) % 9 + 1);			//应作业要求，矩阵第一个位置固定为6 
+	else
+		for (int i = 1; i <= 9; i++)
+			number.push_back(i);										//将1-9装入容器 
 	while (!number.empty()) {
-		int randindex = rand() % number.size();
-		int randnum = number[randindex];
-		number.erase(number.begin() + randindex);
-		num[row][col] = randnum;
+		int randindex = rand() % number.size();  				//随机产生1-9里的 1 个 数字 
+		int randnum = number[randindex];   						//将随机数作为number容器的一个索引指针，将索引位置的数据保存在randnum里
+		number.erase(number.begin() + randindex);  				//删除索引位置的数据 
+		num[row][col] = randnum;  								//将数据填在第row行，第col列 
 		if (!IsSuitable(row, col))
-			continue;
-		if (row == endrow && col == endcol) {
+			continue;  											//如果 randnum不能填在number[row][col]这个位置，则继续循环找一个合适的数 
+		if (row == SIZE - 1 && col == SIZE - 1) {
 			return true;
-		}
-		int nextrow, nextcol;
-		if (col == endcol) {
-			nextrow = row + 1;
-			nextcol = col;
-		}
+		}  														//如果最右下角的空也填上了，返回ture，成功生成数独矩阵  
+		if (col == SIZE - 1) {
+			nextrow = ++row;
+			nextcol = 0;
+		}  														//如果探索到最后一列，则换行 
 		else {
 			nextrow = row;
-			nextcol = col + 1;
-		}
-		bool next = generate(nextrow, nextcol, endrow, endcol);
-		if (next)  return true;
+			nextcol = ++col;
+		}  														//nextrow，nextcol指向下一个空格 
+		bool next = generate(nextrow, nextcol);						//递归遍历整个数独矩阵  
+		if (next)  return true; 								 	//当返回ture时 矩阵成功生成
 	}
 	if (number.empty()) {
 		num[row][col] = 0;
 		return false;
-	}
-}  																			//为指定3X3矩阵赋值 
+	} 															//生成的时候卡住了便回溯上一层 
 
+}
 
-void output() {
+void outputTocmd() {
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
 			printf(" %d", num[i][j]);
 		}
 		printf("\n");
 	}
+	printf("-------------------------------");
+	printf("\n");
+}
+//打印输出到屏幕
+void outputTotext() {
 
-}  //打印输出 
+
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			ocout << num[i][j] << " ";
+		}
+		ocout << "\n";
+	}
+	ocout << "-------------------------------";
+	ocout << "\n";
+}
+//打印输出到文件
+
 int main() {
-
-
-	init();
+	int N;
+	ocout.open("data.txt");
+	printf("请输入您要生成的数独矩阵个数:\n");
+	int CharJduge = scanf_s("%d", &N);
+	if (!CharJduge) {
+		printf("输入只能为数字,请重新输入\n");
+		system("pause");
+		return 0;
+	}
 	srand((unsigned)time(NULL));
-	output();
-	cout << "--------------------------" << endl;
-	generate(0, 6, 2, 8);
-	output();
 
+	ocout << "您请求的生成的" << N << "个随机数独矩阵如下:" << endl;
+	for (int i = 0; i<N; i++) {
+		generate(0, 0); 													//从0,0位置开始遍历生成随机数独矩阵 
+		outputTotext();
+		outputTocmd();
+	}
+	ocout.close();
 	system("pause");
 	return 0;
 }
-
